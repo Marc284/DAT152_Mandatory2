1bfaca2dcff33f9b0d6b46c02a7665ab2661331c
alex <alejandro.lliso.cosin@gmail.com>
2018-09-19 11:24:00 +0200

    Edit and Delete button

--- UIHandler.js
+++ UIHandler.js
@@ -17,17 +17,43 @@ class UIHandler {
 
         get length() {return this.memberList.length;}
 
+
+        deleteMember(memberId){
+            this.memberlist.deleteRow(memberId - 1)
+        }
+
+        editMember(memberId,memberEdit){        
+            if(memberEdit[0] != "")
+                this.memberlist.rows[memberId-1].cells[0].textContent = memberEdit[0]
+            if(memberEdit[1] != "")
+                this.memberlist.rows[memberId-1].cells[1].textContent = memberEdit[1]
+            if(memberEdit[2] != "")
+                this.memberlist.rows[memberId-1].cells[2].textContent = memberEdit[2]
+            if(memberEdit[3] != "")
+                this.memberlist.rows[memberId-1].cells[3].textContent = memberEdit[3]
+        }
+    
+
         addMember(member) {
         var included = this.memberIds.includes(member.memberid);
         if(!included) {
             this.memberList.push(member);
             this.memberIds.push(member.memberid);
             var row = this.tbody.insertRow(-1);
-            row.insertCell(-1).innerHTML = member.firstname;
-            row.insertCell(-1).innerHTML = member.lastname;
-            row.insertCell(-1).innerHTML = member.address;
-            row.insertCell(-1).innerHTML = member.phone;
-            row.insertCell(-1);
+            row.insertCell(-1).textContent = member.firstname;
+            row.insertCell(-1).textContent = member.lastname;
+            row.insertCell(-1).textContent = member.address;
+            row.insertCell(-1).textContent = member.phone;
+
+            let editBtn = document.createElement('input');
+            editBtn.type ="button";
+            editBtn.value ="Edit";
+            row.appendChild(editBtn);
+
+            let deleteBtn = document.createElement('input')
+            deleteBtn.type = "button";
+            deleteBtn.value = "Delete"
+            row.appendChild(deleteBtn);
         }
         else console.log("Id already used"); 
         }
